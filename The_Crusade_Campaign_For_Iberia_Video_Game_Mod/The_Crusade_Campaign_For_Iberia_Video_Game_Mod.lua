619857e67d5fcb3afdc86477d14967249e92554b
Daniel-Hanrahan-Tools-and-Games <106982630+Daniel-Hanrahan-Tools-and-Games@users.noreply.github.com>
2025-08-04 22:12:17 -0400

    The mod file itself

---
+++ The_Crusade_Campaign_For_Iberia_Video_Game_Mod/The_Crusade_Campaign_For_Iberia_Video_Game_Mod.lua
@@ -0,0 +1,11 @@
+-- Notice and Name string
+strNoticeAndName = "The_Crusade_Campaign_For_Iberia_Mod" .. "\n" .. "Copyright (C) 2025 Daniel Hanrahan Tools and Games" .. "\n" .. "This Expansion or Mod is under the BSD-3-Clause license and everything else in this Expansion/Mod is under this: This work is licensed under CC BY 4.0 \n"
+
+-- Dice integer
+intDice = math.random(1, 2);
+
+-- New Location string
+strNewLocation = "15. Jerusalem"
+
+-- Bad Result string
+strBadResult = "Your army got lost in terrain, Game Over."
